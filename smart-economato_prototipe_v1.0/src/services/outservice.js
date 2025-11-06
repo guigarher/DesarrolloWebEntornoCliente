f55edabc57a282fa92214a9dba73c7eabc2f596d
guigarher <guigarher@gmail.com>
2025-11-06 12:27:22 +0000

    asd

--- smart-economato_prototipe_v1.0/src/services/outservice.js
+++ smart-economato_prototipe_v1.0/src/services/outservice.js
@@ -1,7 +1,14 @@
 const API_URL = "http://localhost:3000";
 
-export async function getUser(){
+export const AuthService = {
+
+    async function login(username, password) {
+        const res = await fetch(`${API_URL}/usuarios.`)
+    }
+}
+
+/*export async function getUser(){
   const res = await fetch(`${API_URL}/usuarios`);
   if (!res.ok) throw new Error("Error al cargar usuarios");
   return await res.json();
-} 
+} */
