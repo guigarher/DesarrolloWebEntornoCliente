2a3761918e750d6f5adb9811637c733cbe0900c8
guigarher <guigarher@gmail.com>
2026-01-15 12:26:17 +0000

    asd

--- REACT/react-app/src/Titulo.tsx
+++ REACT/react-app/src/Titulo.tsx
@@ -1,15 +1,11 @@
-import Titulo from "./Titulo"
-import './App.css'
-
-function App() {
+function Titulo() {
   //Esto es JSX. Parecido al HTML pero con restricciones ---> React.createElement("p","Hola Mundo") babel.js pagina que te traduce este JSX y te enseña lo que esta haciendo REACT que es lo del comentario más o menos
-  /*const nombre = "Clase"
-
+  const nombre = "Clase"
+  
   if(nombre){
-    return <p>Hola {nombre}!</p>
+    return <h1>Hola {nombre}!</h1>
   }
-  return <p>Hola Mundo</p>*/
-  return <Titulo></Titulo>
+  return <h1>Hola Mundo</h1>
 }
 
-export default App
+export default Titulo
