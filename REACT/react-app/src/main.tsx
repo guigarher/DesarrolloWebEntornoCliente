af92f1f0dde781e4a92a3999bcb1bfb911d11c78
guigarher <guigarher@gmail.com>
2026-02-04 12:08:58 +0000

    asd

--- REACT/react-app/src/main.tsx
+++ REACT/react-app/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 //Importamos un css de bootstrap de los que hemos instalado
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.tsx";
+import App from "./App_old.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
